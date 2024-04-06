b392cef76ce9d3ebfbc07ede226c69ab300eea2e
Peter Sequeira <pitsix23@gmail.com>
2024-04-05 20:32:26 -0600

    React-Hooks

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Semaphor from './semaforo';
+
+const App = () => {
+  return (
+    <div>
+      {/* Otros componentes de tu aplicación */}
+      <Semaphor />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/semaforo.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './semaforo.css';
+
+const Semaphor = () => {
+
+  const [color, setColor] = useState('...');
+  const [lastSelectedColor, setLastSelectedColor] = useState(null);
+
+  return (
+    <div>
+      <div className="semaphor">
+        <div className="black-rectangle">
+          <button
+            className={`light ${color === 'red' || lastSelectedColor === 'red' ? 'active' : ''}`}
+            data-color="red"
+            onClick={() => {
+              if (color === 'red') { 
+                setColor(null);
+                setLastSelectedColor(null);
+              } else {
+                setColor('red'); 
+                setLastSelectedColor('red');
+              }
+            }}
+          />
+          <button
+            className={`light ${color === 'yellow' || lastSelectedColor === 'yellow' ? 'active' : ''}`}
+            data-color="yellow"
+            onClick={() => {
+              if (color === 'yellow') { 
+                setColor(null);
+                setLastSelectedColor(null);
+              } else {
+                setColor('yellow'); 
+                setLastSelectedColor('yellow');
+              }
+            }}
+          />
+          <button
+            className={`light ${color === 'green' || lastSelectedColor === 'green' ? 'active' : ''}`}
+            data-color="green"
+            onClick={() => {
+              if (color === 'green') { 
+                setColor(null);
+                setLastSelectedColor(null);
+              } else {
+                setColor('green'); 
+                setLastSelectedColor('green');
+              }
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Semaphor;
